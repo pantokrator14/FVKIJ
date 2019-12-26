07d5f17b2028f728a1238590f0e540050d37bb92
Julio José Adam Martínez <julioblogs1998@gmail.com>
2019-12-26 05:44:27 +0000

    Updated comments on auth.js

--- src/helpers/auth.js
+++ src/helpers/auth.js
@@ -1,12 +1,12 @@
 //Esto nos permite autenticar al usuario cada vez que este desee realizar una accion
-const helpers = {};
+const helpers = {}; //Creamos una variable llamada helpers
 
-helpers.isAuthenticated = (req, res, next) => {
-  if (req.isAuthenticated()) {
-    return next();
+helpers.isAuthenticated = (req, res, next) => { //A la cual creamos un metodo
+  if (req.isAuthenticated()) { //Si el usuario está autenticado
+    return next(); //Pasa
   }
-  req.flash('error_msg', 'No esta autenticado.');
-  res.redirect('/');
+  req.flash('error_msg', 'No esta autenticado.'); //Sino, error.
+  res.redirect('/'); //Y regresa a la pantalla de login
 };
-
+//Finalmente exportamos esto para ser usado en otros archivos.
 module.exports = helpers;
