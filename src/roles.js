182d2699673e6d2e51fa65c2d8dea6109b60c523
Julio José Adam Martínez <julioblogs1998@gmail.com>
2021-01-15 10:47:40 -0400

    chaged html

--- src/roles.js
+++ src/roles.js
@@ -1,8 +1,9 @@
 //Acá se controlan los roles de acceso por parte de cada usuario
 const accesscontrol = require("accesscontrol"); //Solicitamos la dependencia necesaria
 
-const ac = new accesscontrol();
+const ac = new accesscontrol(); //creamos la instancia en la que te estableceran los roles
 
 exports.roles = (function(){
-    
+    ac.grant('secretario')
+        
 })();
